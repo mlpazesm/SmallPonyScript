68112aa494810645a09489e46f0d8ec8ed98e5e0
mlpazesm <das@akfasfasf.ru>
2014-06-30 18:36:25 +1000

    Update smallpony.user.js

--- smallpony.user.js
+++ smallpony.user.js
@@ -116,7 +116,6 @@ selectNamedMessage(user_name);
 if (document.URL.indexOf("%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8") != -1) {
     console.log("Вы на новостной странице");
 } else if (document.URL.indexOf("%D0%BE%D0%B1%D1%80%D0%B0%D1%89%D0%B5%D0%BD%D0%B8%D0%B5") != -1) {
-    console.log("asd");
     $("div.greeting").html("Добро пожаловать в Обращения. <br>Обратите внимание! Это неофициальная функция!");
     unsafeWindow.$(window).unbind('scroll');
 //Код на десяточку!
