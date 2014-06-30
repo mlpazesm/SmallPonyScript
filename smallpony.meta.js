2641c2c3c5fc2242df9fafe9c024b4c440e91dc1
mlpazesm <das@akfasfasf.ru>
2014-06-30 22:11:22 +1000

    Create  	smallpony.meta.js

---
+++ smallpony.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name SmallPonyScript
+// @version 0.14.88b
+// @namespace http://азъесмь.рф/
+// @author @млп (понилюб)
+// @description VerySmallPonyScript
+// @include http://азъесмь.рф/*
+// @include http://xn--80akfvy6cr.xn--p1ai/*
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant unsafeWindow
+// ==/UserScript==
